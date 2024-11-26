ff170afc0bba49e1c3ea1004b8b28f770d30b197
FaizFd <69901327+FaizFd@users.noreply.github.com>
2024-11-26 11:21:06 -0500

    Add Frontend NextJs and Backend NodeJs

--- social-media-platform/client/src/constants.tsx
+++
@@ -1,5 +0,0 @@
-// client/src/constants.ts
-
-export const token: string | null = localStorage.getItem('token');
-export const username: string | null = localStorage.getItem('username');
-export const apiBaseUrl: string | undefined = process.env.REACT_APP_API_BASE_URL;
